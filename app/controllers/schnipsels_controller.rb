48d4f8401761c499879c734fab56f28d060b05a7
Vincent Eberle <eberle@wlp-systems.de>
2017-06-20 11:05:06 +0200

    Benutzer Authentifizierung und Navigation

--- app/controllers/schnipsels_controller.rb
+++ app/controllers/schnipsels_controller.rb
@@ -1,5 +1,6 @@
 class SchnipselsController < ApplicationController
   before_action :set_schnipsel, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /schnipsels
   # GET /schnipsels.json
