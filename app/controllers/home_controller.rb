ee1535728cfc32de02fd948df602fd52c95cf7a1
Vincent Eberle <eberle@wlp-systems.de>
2017-06-20 19:49:19 +0200

    Tag Cloud

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,14 +2,17 @@ class HomeController < ApplicationController
 
   def index
     @schnipsels = Schnipsel.all
+    @tags = Schnipsel.tag_counts_on(:tags)
   end
 
   def search
     @schnipsels = Schnipsel.tagged_with(suche_params[:tag_list].split(','), :any => true)
+    @tags = Schnipsel.tag_counts_on(:tags)
     render "home/index"
 
   end
 
+  private
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def suche_params
